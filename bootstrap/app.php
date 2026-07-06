72f6fb65a11edb684aac3432fb3c60a4455295ca
Takochi23 <gagahmank@gmail.com>
2026-07-06 21:31:58 +0700

    temp disable csrf on ajax/transactions for debugging

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->validateCsrfTokens(except: [
+            'ajax/transactions/*',
+            'ajax/transactions',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
